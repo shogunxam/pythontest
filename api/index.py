d8e1c0d5620b578efe01158c49a8189930a12937
MARIO D'AMICO <mario.damico75@gmail.com>
2023-02-19 15:18:58 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -16,11 +16,11 @@ from phishingkiller import start_process
 import threading
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        t = threading.Thread(name='start_process', target=start_process)
-        t.setDaemon(True)
-        t.start()
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
+        t = threading.Thread(name='start_process', target=start_process, args=(self.wfile.write,))
+        t.setDaemon(True)
+        t.start()
         return
