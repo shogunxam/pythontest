8a353c479e9ca591bec723d520b0a3b2ab9fe228
MARIO D'AMICO <mario.damico75@gmail.com>
2023-02-19 15:32:52 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -12,15 +12,30 @@
 
 
 from http.server import BaseHTTPRequestHandler
-from phishingkiller import start_process
+from phishingkiller import start_process, getLatestValue
 import threading
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        t = threading.Thread(name='start_process', target=start_process, args=(self.wfile.write,))
-        t.setDaemon(True)
-        t.start()
+        path = self.path.split("?",1)[0]
+        if path == '/state':
+             self.send_response(200)
+            self.send_header('Content-type','text/plain')
+            self.end_headers()
+            self.wfile.write(getLatestValue().encode('utf-8'))
+        elif path == '/start':
+            self.send_response(200)
+            self.send_header('Content-type','text/plain')
+            self.end_headers()
+            self.wfile.write('Starting...'.encode('utf-8'))
+            t = threading.Thread(name='start_process', target=start_process, args=(self.wfile.write,))
+            t.setDaemon(True)
+            t.start()
+        else:
+            self.send_response(200)
+            self.send_header('Content-type','text/plain')
+            self.end_headers()
+            self.wfile.write('Hello, world!'.encode('utf-8'))
+            t = threading.Thread(name='start_process', target=start_process, args=(self.wfile.write,))
+            t.setDaemon(True)
+            t.start()
         return
