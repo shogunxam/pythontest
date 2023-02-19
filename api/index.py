17d674f5c0d406d4f0a33f948493b9d01ee0ddef
MARIO D'AMICO <mario.damico75@gmail.com>
2023-02-19 14:38:15 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,26 @@
-from flask import Flask
+#from flask import Flask
+#from phishingkiller import start_proces
+#import threading
+#app = Flask(__name__)
+
+#@app.route('/')
+#def home():    
+#    t = threading.Thread(name='start_proces', target=start_proces)
+#    t.setDaemon(True)
+#    t.start()
+#    return 'Process running'
+
+
+from http.server import BaseHTTPRequestHandler
 from phishingkiller import start_proces
 import threading
-app = Flask(__name__)
-
-@app.route('/')
-def home():    
-    t = threading.Thread(name='start_proces', target=start_proces)
-    t.setDaemon(True)
-    t.start()
-    return 'Process running'
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        t = threading.Thread(name='start_proces', target=start_proces)
+        t.setDaemon(True)
+        t.start()
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
