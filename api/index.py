b0adb809ae273f0b2fb365d81e7e46ab4217a84b
MARIO D'AMICO <mario.damico75@gmail.com>
2023-02-19 14:25:05 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,14 +1,15 @@
 from http.server import BaseHTTPRequestHandler
 from phisingkiller import start_proces
 import threading
-class handler(BaseHTTPRequestHandler):
+    
+from flask import Flask
+from phisingkiller import start_proces
+import threading
+app = Flask(__name__)
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        t = threading.Thread(name='start_proces', target=start_proces)
-        t.setDaemon(True)
-        t.start()
-        return
+@app.route('/')
+def home():    
+    t = threading.Thread(name='start_proces', target=start_proces)
+    t.setDaemon(True)
+    t.start()
+     return 'Process running'
